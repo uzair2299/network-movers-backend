d02ef03a42eac716393d46353e52da7b7e5d09ef
uzair <uzair.anwar@expertflow.com>
2026-05-24 10:31:25 +0500

    fix(db): remove custom depends-on configuration and resolve startup lifecycle exceptions

--- src/main/java/com/company/networkmovers/config/database/JpaAuditingConfig.java
+++ src/main/java/com/company/networkmovers/config/database/JpaAuditingConfig.java
@@ -3,7 +3,6 @@ package com.company.networkmovers.config.database;
 import com.company.networkmovers.security.context.CustomUserDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
@@ -13,7 +12,6 @@ import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-@DependsOn("flyway")
 public class JpaAuditingConfig {
 
     @Bean
